2e4dfcf98e78599fc3fe55e472895c97ae11e79b
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-23 15:39:59 +0200

    Category seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,41 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            [
+                'name' => 'Sport'
+            ],
+            [
+                'name' => 'Politica'
+            ],
+            [
+                'name' => 'Attualità'
+            ],
+            [
+                'name' => 'Cronaca'
+            ],
+            [
+                'name' => 'Tecnologia'
+            ],
+            [
+                'name' => 'Estero'
+            ],
+            [
+                'name' => 'Istruzione'
+            ],
+            [
+                'name' => 'Cultura'
+            ],
+            [
+                'name' => 'Economia'
+            ],
+            [
+                'name' => 'Curiosità'
+            ],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
